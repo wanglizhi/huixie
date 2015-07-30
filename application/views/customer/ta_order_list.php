ab9b862e2d8404ee0de15d809644e55562d7da71
wanglizhi <729699036@qq.com>
2015-07-30 15:53:48 +0800

    修改手机端查看订单列表界面

--- application/views/customer/ta_order_list.php
+++ application/views/customer/ta_order_list.php
@@ -21,7 +21,7 @@
 		<div class="row-fluid">
 			<div class="span12">
 				<h3 class="page-title">
-					订单列表
+					接单列表
 				</h3>
 				<ul class="breadcrumb">
 					<li>
@@ -49,16 +49,15 @@
 
 					<div class="portlet-body">
 						<div>
-							<table class="table" id="orderList">
-								<thead>
+							<table class="table table-bordered table-striped table-condensed cf" id="orderList">
+								<thead class="cf">
 									<tr>
 										<th>订单编号</th>
-										<th>用户编号</th>
-										<th>TA编号</th>
 										<th>专业</th>
 										<th>课程名称</th>
+										<th>页数</th>
+										<th>补充材料</th>
 										<th>截止日期</th>
-										<th>价格</th>
 										<th>订单状态</th>
 										<th>详情</th>
 									</tr>
@@ -66,23 +65,21 @@
 								<tbody>
 									<?php if(!empty($orderList))foreach ($orderList as $order):?>
 									<tr>
-										<td> <?php echo $order['orderNum'];?> </td>
-										<td> <?php echo $order['userId'];?> </td>
-										<td> <?php echo $order['taId'];?> </td>
-										<td> <?php echo $order['major'];?> </td>
-										<td> <?php echo $order['courseName'];?> </td>
-										<td> <?php echo $order['finishedTime'];?> </td>
-										<td> <?php echo $order['price'];?> </td>
-										<?php if($order['hasPaid']==0): ?>
-										<td> <span class="label label-default">未付款</span></td>
-									<?php elseif($order['hasTaken']==0): ?>
-									<td> <span class="label label-warning">已付款</span></td>
-								<?php elseif($order['hasFinished']==0): ?>
-								<td> <span class="label label-info">已接单</span></td>
-							<?php else: ?>
-							<td> <span class="label label-success">已完成</span></td>
-						<?php endif; ?>
-						<td>
+										<td data-title="订单编号"> <?php echo $order['orderNum'];?> </td>
+										<td data-title="专业"> <?php echo $order['major'];?> </td>
+										<td data-title="课程名称"> <?php echo $order['courseName'];?> </td>
+										<td data-title="页数"> <?php echo $order['pageNum'];?> </td>
+										<td data-title="补充材料"> <?php echo $order['refDoc'];?> </td>
+										<td data-title="截止日期"> <?php echo $order['endTime'];?> </td>
+										<?php if($order['hasTaken']==0): ?>
+										<td data-title="状态"> <span class="label label-default">未接单</span></td>
+									<?php elseif($order['hasFinished']==0): ?>
+									<td data-title="状态"> <span class="label label-success">已接单</span></td>
+								<?php else: ?>
+								<td data-title="状态"> <span class="label label-info">已完成</span></td>
+								<?php endif; ?>
+
+						<td data-title="详情">
 							<?php 
 							static $orderRow = 0; 
 							?>
@@ -97,18 +94,13 @@
 									<div class="profile-classic row-fluid">
 										<ul style="width:100%;" class="unstyled span10">
 											<li style="width:100%;"><span>订单编号:</span> <?=$order['orderNum']?></li>
-											<li style="width:100%;"><span>用户编号:</span> <?=$order['userId']?></li>
 											<li style="width:100%;"><span>专业:</span> <?=$order['major']?></li>
 											<li style="width:100%;"><span>课程名称:</span> <?=$order['courseName']?></li>
 											<li style="width:100%;"><span>邮箱:</span> <?=$order['email']?></li>
 											<li style="width:100%;"><span>页数:</span> <?=$order['pageNum']?></li>
 											<li style="width:100%;"><span>阅读材料:</span> <?=$order['refDoc']?></li>
 											<li style="width:100%;"><span>截止日期:</span> <?=$order['endTime']?></li>
-											<li style="width:100%;"><span>价格:</span> <?=$order['price']?></li>
 											<li style="width:100%;"><span>额外需求:</span> <?=$order['requirement']?></li>
-											<li style="width:100%;"><span>TA编号:</span> <?=$order['taId']?></li>
-											<li style="width:100%;"><span>创建时间:</span> <?=$order['createTime']?></li>
-											<li style="width:100%;"><span>付款时间:</span> <?=$order['paidTime']?></li>
 											<li style="width:100%;"><span>接单时间:</span> <?=$order['takenTime']?></li>
 											<li style="width:100%;"><span>结束时间:</span> <?=$order['finishedTime']?></li>
 										</ul>
