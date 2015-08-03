feecece1d754dc888520d38cc8bea6ec7be17db7
wanglizhi <729699036@qq.com>
2015-08-03 12:46:22 +0800

    修改notifyUrl

--- application/controllers/customer/Order.php
+++ application/controllers/customer/Order.php
@@ -164,11 +164,63 @@ class Order extends CustomerController {
 	}
 	function notifyUrl(){
 		$this->log('notifyUrl start <-----------------------------------------------');
+
+		// read the post from PayPal system and add 'cmd'  
+		$req = 'cmd=_notify-validate';  
+		   
+		foreach ($_POST as $key => $value) {  
+		$value = urlencode(stripslashes($value));  
+		$req .= "&$key=$value";  
+		}  
+		   
+		// post back to PayPal system to validate  
+		$header .= "POST /cgi-bin/webscr HTTP/1.0\r\n";  
+		$header .= "Content-Type: application/x-www-form-urlencoded\r\n";  
+		$header .= "Content-Length: " . strlen($req) . "\r\n\r\n";  
+		   
+		$fp = fsockopen ('ssl://www.sandbox.paypal.com', 443, $errno, $errstr, 30); // 沙盒用  
+		//$fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30); // 正式用  
+		   
+		// assign posted variables to local variables  
+		$item_name = $_POST['item_name'];  
+		$item_number = $_POST['item_number'];  
+		$payment_status = $_POST['payment_status'];  
+		$payment_amount = $_POST['mc_gross'];  
+		$payment_currency = $_POST['mc_currency'];  
+		$txn_id = $_POST['txn_id'];  
+		$receiver_email = $_POST['receiver_email'];  
+		$payer_email = $_POST['payer_email'];  
+		$mc_gross = $_POST['mc_gross ']; // 付款金额  
+		$custom = $_POST['custom ']; // 得到订单号  
+
+		$this->log($item_number);
+		   
+		if (!$fp) {  
+		// HTTP ERROR  
+			$this->log('http error');
+		} else {  
+		fputs ($fp, $header . $req);  
+		while (!feof($fp)) {  
+		$res = fgets ($fp, 1024);  
+		if (strcmp ($res, "VERIFIED") == 0) {  
+		// check the payment_status is Completed  
+		// check that txn_id has not been previously processed  
+		// check that receiver_email is your Primary PayPal email  
+		// check that payment_amount/payment_currency are correct  
+		// process payment  
+		// 验证通过。付款成功了，在这里进行逻辑处理（修改订单状态，邮件提醒，自动发货等）  
+			$this->log('VERIFIED');
+			$this->log('payment_status->'.$_POST['payment_status']);
+		}  
+		else if (strcmp ($res, "INVALID") == 0) {  
+		// log for manual investigation  
+		// 验证失败，可以不处理。  
+			$this->log('INVALID');
+		}  
+		}  
+		fclose ($fp);  
+		}
 		// var_dump($_POST);
-		$this->log($_POST['business']);
-		$this->log($_POST['item_name']);
-		$this->log($_POST['item_number']);
-		$this->log($_POST['payment_status']);
 		$this->log('notifyUrl end <-----------------------------------------------');
 	}
 
