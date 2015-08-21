fded8fde84e4cc900d3b89f641ced049008340c0
wanglizhi <729699036@qq.com>
2015-08-21 16:32:00 +0800

    fix a bug

--- application/controllers/customer/Order.php
+++ application/controllers/customer/Order.php
@@ -197,9 +197,8 @@ class Order extends CustomerController {
 
 
 		//初始化微信数据
-		$result = $this->wxpay($user['openid'], $sessionId, $orderNum, $max);
-		$data['jsApiParameters'] = $result['jsApiParameters'];
-		$data['editAddress'] = $result['editAddress'];
+		$jsApiParameters = $this->wxpay($user['openid'], $sessionId, $orderNum, $max);
+		$data['jsApiParameters'] = $jsApiParameters;
 
 
 		$this->loadView('pay_order_detail',$data);
@@ -288,7 +287,7 @@ class Order extends CustomerController {
         $jsApiParameters = $tools->GetJsApiParameters($order);
 
         //获取共享收货地址js函数参数
-        $editAddress = $tools->GetEditAddressParameters();
+        // $editAddress = $tools->GetEditAddressParameters();
 
         //③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
         /**
@@ -297,10 +296,8 @@ class Order extends CustomerController {
          * 2、jsapi支付时需要填入用户openid，WxPay.JsApiPay.php中有获取openid流程 （文档可以参考微信公众平台“网页授权接口”，
          * 参考http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html）
          */
-        $data['jsApiParameters'] = $jsApiParameters;
-        $data['editAddress'] = $editAddress;
         // $this->load->view('customer/jsapi_page',$data);
-        return $data;
+        return $jsApiParameters;
 	}
 
 }
--- application/views/customer/pay_order_detail.php
+++ application/views/customer/pay_order_detail.php
@@ -216,34 +216,5 @@
 	            jsApiCall();
 	        }
 	    }
-	    //获取共享地址
-	    function editAddress()
-	    {
-	        WeixinJSBridge.invoke(
-	            'editAddress',
-	            <?php echo $editAddress; ?>,
-	            function(res){
-	                var value1 = res.proviceFirstStageName;
-	                var value2 = res.addressCitySecondStageName;
-	                var value3 = res.addressCountiesThirdStageName;
-	                var value4 = res.addressDetailInfo;
-	                var tel = res.telNumber;
-	                
-	                alert(value1 + value2 + value3 + value4 + ":" + tel);
-	            }
-	        );
-	    }
-	    window.onload = function(){
-        // if (typeof WeixinJSBridge == "undefined"){
-        //     if( document.addEventListener ){
-        //         document.addEventListener('WeixinJSBridgeReady', editAddress, false);
-        //     }else if (document.attachEvent){
-        //         document.attachEvent('WeixinJSBridgeReady', editAddress); 
-        //         document.attachEvent('onWeixinJSBridgeReady', editAddress);
-        //     }
-        // }else{
-        //     // editAddress();
-        // }
-    	};
 
 		</script>
